7c3c1077fc89303a55d6b14a783cd5d0cf998d43
Pavel <Pavel>
2019-02-01 23:00:13 +0300

    Add constructor

--- AverageHash.php
+++ AverageHash.php
@@ -8,14 +8,28 @@ class AverageHash implements AverageHashInterface
     public $inputImage;
     public $hash;
 
-    public function __construct()
+    public function __construct($inputImage)
     {
-
+        if (file_exists($inputImage)) {
+            // get extension
+            $ext = explode('.', $inputImage);
+            $ext = $ext[count($ext) - 1];
+            // get function name
+            $funcName = 'imagecreatefrom' . $ext;
+            if (function_exists($funcName)) {
+                $this->inputImage = $funcName($inputImage);
+            } else {
+                throw new \Error("Cannot find function" . $funcName);
+            }
+        } else {
+            throw new \Error("Cannot find image");
+        }
     }
 
     public function resizeImage()
     {
         // TODO: Implement resizeImage() method.
+//        if (imagecopyresized())
     }
 
     public function imageToGray()
